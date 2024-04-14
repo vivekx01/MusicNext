8e27af19e900b5fc70de18528911a22147a526e5
Vivek <viveks95944@gmail.com>
2024-04-14 15:51:56 +0530

    card_and_parallax_added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
-export default function Home() {
+function Home() {
   return (
     <div>
       <HeroSection></HeroSection>
+      <FeaturedCourses></FeaturedCourses>
+      <WhyChooseUs></WhyChooseUs>
     </div>
   );
 }
+
+export default Home;
