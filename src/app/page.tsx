f3eeb27be9db69330afc2165546a7e32721d743b
Vivek <viveks95944@gmail.com>
2024-04-14 14:52:43 +0530

    herosection_added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
-
+import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
-    <h1 className="text-2xl text-center">Vivek</h1>
+    <div>
+      <HeroSection></HeroSection>
+    </div>
   );
 }
