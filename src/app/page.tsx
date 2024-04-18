6ba4d853da967ed7abcc3ab286b15c9f34512c30
Vivek <viveks95944@gmail.com>
2024-04-18 08:42:52 +0530

    wavy_background_added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
 import TestimonialCards from "@/components/TestimonialCards";
 import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
@@ -12,6 +13,7 @@ function Home() {
       <WhyChooseUs></WhyChooseUs>
       <TestimonialCards></TestimonialCards>
       <UpcomingWebinars></UpcomingWebinars>
+      <Instructors></Instructors>
     </div>
   );
 }
