e6b66b7367e13e33bca484299945d57846abe1c4
Vivek <viveks95944@gmail.com>
2024-04-21 08:50:36 +0530

    config_changed_for_images

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export', // This enables static export
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'images.unsplash.com',
-          port: '',
-          pathname: '**',
-        },
-        {
-          protocol: 'https',
-          hostname: 'res.cloudinary.com',
-          port: '',
-          pathname: '**',
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
-  
+  output: 'export', // Enables static export
+  basePath: '/musicnext', // Sets the base path to /musicnext
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
