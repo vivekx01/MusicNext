4c32f76af86a5e6363aeb27d640cf5cbdfec6a63
Vivek <viveks95944@gmail.com>
2024-04-21 09:36:26 +0530

    images_fix_2

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // Enables static export
-  
+  basePath: '/musicnext',
   images: {
     unoptimized: true, // Disables image optimization
     remotePatterns: [
